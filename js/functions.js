f9aa26718883fe17584bd153c857220237014539
Роман Попов <romapopob.2005@gmail.com>
2024-10-25 20:21:02 +0500

    module5-task2

--- js/functions.js
+++ js/functions.js
@@ -1,46 +1,16 @@
-function checkLength(str, maxLenght){
-  return str.length <= maxLenght;
+function isMeetingPossible(startDay, endDay, startMeeting, durationMeeting){
+  startDay = getRefactorValueInMinutes(startDay);
+  endDay = getRefactorValueInMinutes(endDay);
+  startMeeting = getRefactorValueInMinutes(startMeeting);
+  return startMeeting >= startDay && startMeeting + durationMeeting <= endDay;
 }
-checkLength('проверяемая строка',20);
-checkLength('проверяемая строка',18);
-checkLength('проверяемая строка',10);
-
-function checkPalindrome(str){
-  const palindrome = str.toLowerCase().replaceAll(' ','');
-  let flag = true;
-  for (let i = 0; i < Math.floor(palindrome.length / 2);i++){
-    flag = palindrome[i] === palindrome[palindrome.length - 1 - i];
-    if (!flag){
-      return false;
-    }
-  }
-  return flag;
+function getRefactorValueInMinutes(string){
+  const stringList = string.split(':').map((element)=> +element);
+  return stringList[0] * 60 + stringList[1];
 }
-checkPalindrome('топот');
-checkPalindrome('ДовОд');
-checkPalindrome('топ о   т');
-checkPalindrome('Кекс');
-checkPalindrome('Лёша на полке клопа нашёл ');
-checkPalindrome('отто');
 
-function cureNumber(parameter){
-  const str = parameter.toString();
-  let numBuilder = '';
-  for (let i = 0;i < str.length; i++){
-    const number = parseInt(str[i],10);
-    numBuilder += !Number.isNaN(number) ? str[i] : '';
-  }
-  if (numBuilder === ''){
-    return NaN;
-  }
-  return parseInt(numBuilder,10);
-}
-cureNumber('2023 год');
-cureNumber('ECMAScript 2022');
-cureNumber('1 кефир, 0.5 батона');
-cureNumber('агент 007');
-cureNumber('а я томат');
-cureNumber(2023);
-cureNumber(-1);
-cureNumber(1.5);
-
+isMeetingPossible('08:00', '17:30', '14:00', 90);
+isMeetingPossible('8:0', '10:0', '8:0', 120);
+isMeetingPossible('08:00', '14:30', '14:00', 90);
+isMeetingPossible('14:00', '17:30', '08:0', 90);
+isMeetingPossible('8:00', '17:30', '08:00', 900);
